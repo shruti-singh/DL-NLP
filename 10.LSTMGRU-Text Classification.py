84887862764cafa9e8a31f38c23d0d3f59eefe08
shruti <shrutis1051@gmail.com>
2019-12-31 04:14:39 +0530

    fix plots and cleanup

--- 10.LSTMGRU-Text Classification.py
+++ 10.LSTMGRU-Text Classification.py
@@ -1,6 +1,18 @@
 import tensorflow as tf 
 print(tf.__version__)
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print("GPUs: ", gpus)
+if gpus:
+	try:
+		# Restrict TensorFlow to only use the second GPU
+		tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
+		logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+		print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+		print("Using GPU:1")
+	except RuntimeError as e:
+		print(e)
+
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,12 +54,12 @@ class Reg:
 	
 	@staticmethod
 	def l2_reg(weight_matrix):
-		return 0.01 * K.sum(K.square(x))
+		return 0.01 * 0.5 * K.sum(K.square(x))
 
 
 
 # Dataset preparation
-train_validation_split = tfds.Split.TRAIN.subsplit([7, 3])
+train_validation_split = tfds.Split.TRAIN.subsplit([6, 4])
 ((train_dataset, validation_dataset), test_dataset), info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True, data_dir=data_dir_path, download=False, split=(train_validation_split, tfds.Split.TEST))
 encoder = info.features["text"].encoder
 print("\n Vocabulary size: ", encoder.vocab_size)
@@ -86,8 +98,9 @@ class MultiLayerLSTM:
 		
 		METRIC_ACCURACY = 'accuracy'
 		
-		self.timestamp = time()
-		self.log_dir = "./tf_logs/lstm_classification/" + str(self.timestamp) +"_model/"
+		self.timestamp = int(time())
+		print("MODEL INIT TIME: ", str(self.timestamp))
+		self.log_dir = "./tf_logs/lstm_classification_" + str(self.timestamp) +"/"
 		with tf.summary.create_file_writer(self.log_dir).as_default():
 			hp.hparams_config(hparams=[self.optimizer, self.num_units_l1, self.num_units_l2, self.num_units_l3, self.dropout], metrics=[hp.Metric(METRIC_ACCURACY, display_name='Accuracy')],)
 		
@@ -98,7 +111,7 @@ class MultiLayerLSTM:
 		for w in self.model.trainable_weights:
 			r += Reg.l1_reg(w)
 		l = LossFunction.binary_crossentropy(y_true, y_pred) + r
-		return l/2.0
+		return l
 	
 #	 def loss_function(self, y_true, y_pred):
 #		 return tf.keras.losses.binary_crossentropy(y_true, y_pred)
@@ -127,39 +140,42 @@ class MultiLayerLSTM:
 		return self.model
 	
 	def save_model(self):
-		model.save_weights('lstm_classification_' + str(self.timestamp), save_format='tf')
+		cp = int(time())
+		model.save_weights(self.logdir + '/saved_models/model_' + cp, save_format='tf')
 		return
 	
 	def compile_model(self, loss_function, optimizer):
 		self.model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
 		return self.model
 	
-	def train_model(self, hparams, train_data, cross_validation_data):
+	def train_model(self, hparams, train_data, cross_validation_data, run_index):
 		self.generate_model(hparams)
 		self.compile_model(self.loss_function, hparams[self.optimizer])
 		
 		callbacks = [
 			# Early stopping
-			tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4)
+			tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4),
+			tf.keras.callbacks.TensorBoard(log_dir=self.log_dir + run_index),
 		]
 		
-		self.model.fit(train_data, epochs=10, validation_data=cross_validation_data, callbacks=callbacks,verbose=1)
+		self.model.fit(train_data, epochs=3, validation_data=cross_validation_data, callbacks=callbacks,verbose=1)
 		_, accuracy = self.model.evaluate(cross_validation_data)
 		
 		return accuracy
 	
 	def run(self, run_dir, hparams, train_data, cross_validation_data):
+		K.clear_session()
+		run_index = run_dir.split("-")[1]
 		with tf.summary.create_file_writer(run_dir).as_default():
 			# record the values used in this trial
 			hp.hparams(hparams)
-			acc = self.train_model(hparams, train_dataset, validation_dataset)
-			tf.summary.scalar('accuracy', acc, step=1)
+			acc = self.train_model(hparams, train_dataset, validation_dataset, run_index)
+			tf.summary.scalar('accuracy', acc, step=int(run_index))
 		return acc
 	
 	def random_search(self, train, cross_val, seed):
 		rng = random.Random(seed)
-		# modified
-		total_points_explored = 10
+		total_points_explored = 2
 		
 		acc_params = []
 		
@@ -177,7 +193,7 @@ class MultiLayerLSTM:
 		return total_points_explored, acc_params
 	
 	def setup_model(self, hparams, single_layer=True):
-		self.generate_model(params, single_layer)
+		self.generate_model(hparams, single_layer)
 		self.compile_model(self.loss_function, hparams[self.optimizer])
 		return self.model
 	
@@ -198,7 +214,7 @@ m.eval_test(test_dataset)
 class GRUNetwork:
 	
 	def __init__(self, teacher_forcing=False):
-		# number of units in 1st and 2nd LSTM layer, and the next dense layer
+		# number of units in 1st and 2nd GRU layer, and the next dense layer
 		self.num_units_l1 = hp.HParam('num_units_l1', hp.Discrete([32, 64]))
 		self.num_units_l2 = hp.HParam('num_units_l2', hp.Discrete([16, 32, 64]))
 		self.num_units_l3 = hp.HParam('num_units_l3', hp.Discrete([32, 64]))
@@ -215,7 +231,7 @@ class GRUNetwork:
 		
 		METRIC_ACCURACY = 'accuracy'
 		
-		self.timestamp = time()
+		self.timestamp = int(time())
 		self.log_dir = "./tf_logs/gru_classification/" + str(self.timestamp) +"_model/"
 		with tf.summary.create_file_writer(self.log_dir).as_default():
 			hp.hparams_config(hparams=[self.optimizer, self.num_units_l1, self.num_units_l2, self.num_units_l3, self.dropout], metrics=[hp.Metric(METRIC_ACCURACY, display_name='Accuracy')],)
@@ -227,7 +243,7 @@ class GRUNetwork:
 		for w in self.model.trainable_weights:
 			r += Reg.l1_reg(w)
 		l = LossFunction.binary_crossentropy(y_true, y_pred) + r
-		return l/2.0
+		return l
 	
 #	 def loss_function(self, y_true, y_pred):
 #		 return tf.keras.losses.binary_crossentropy(y_true, y_pred)
@@ -256,20 +272,22 @@ class GRUNetwork:
 		return self.model
 	
 	def save_model(self):
-		model.save_weights('gru_classification_' + str(self.timestamp), save_format='tf')
+		cp = int(time())
+		model.save_weights(self.logdir + '/saved_models/model_' + cp, save_format='tf')
 		return
 	
 	def compile_model(self, loss_function, optimizer):
 		self.model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
 		return self.model
 	
-	def train_model(self, hparams, train_data, cross_validation_data):
+	def train_model(self, hparams, train_data, cross_validation_data, run_index):
 		self.generate_model(hparams)
 		self.compile_model(self.loss_function, hparams[self.optimizer])
 		
 		callbacks = [
 			# Early stopping
-			tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4)
+			tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4),
+			tf.keras.callbacks.TensorBoard(log_dir=self.log_dir + run_index),
 		]
 		
 		self.model.fit(train_data, epochs=10, validation_data=cross_validation_data, callbacks=callbacks,verbose=1)
@@ -278,17 +296,18 @@ class GRUNetwork:
 		return accuracy
 	
 	def run(self, run_dir, hparams, train_data, cross_validation_data):
+		K.clear_session()
+		run_index = run_dir.split("-")[1]
 		with tf.summary.create_file_writer(run_dir).as_default():
 			# record the values used in this trial
 			hp.hparams(hparams)
-			acc = self.train_model(hparams, train_dataset, validation_dataset)
-			tf.summary.scalar('accuracy', acc, step=1)
+			acc = self.train_model(hparams, train_dataset, validation_dataset, run_index)
+			tf.summary.scalar('accuracy', acc, step=int(run_index))
 		return acc
 	
 	def random_search(self, train, cross_val, seed):
 		rng = random.Random(seed)
-		# modified
-		total_points_explored = 10
+		total_points_explored = 9
 		
 		acc_params = []
 		
@@ -306,13 +325,13 @@ class GRUNetwork:
 		return total_points_explored, acc_params
 	
 	def setup_model(self, hparams, single_layer=True):
-		self.generate_model(params, single_layer)
+		self.generate_model(hparams, single_layer)
 		self.compile_model(self.loss_function, hparams[self.optimizer])
 		return self.model
 	
 	def eval_test(self, test):
 		_, acc = self.model.evaluate(test)
-		print("Acc on tst set: ")
+		print("Acc on test set: ")
 		print(acc)
 		return
 
